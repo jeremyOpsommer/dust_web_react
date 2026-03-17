48abae3efc3b1f1d2640a2850e64da9266095727
Jeremy Opsommer <jeremy.opsommer@gmail.com>
2026-03-17 16:17:00 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenue dans votre application React</h1>
+        <p>Commencez à éditer <code>src/App.js</code> et sauvegardez pour recharger.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
